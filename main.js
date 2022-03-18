68293d87e2f593983d32db4359d0635da7313e42
Karim <kimo_678@live.com>
2022-03-17 22:59:21 -0400

    Completed random computer function

---
+++ main.js
@@ -0,0 +1,11 @@
+function computerPlay()
+{
+    let play = ["Rock", "Paper", "Scissor"];
+    return play[Math.floor(Math.random() * 3)];
+}
+function playRound(player, computer)
+{
+    
+}
+
+console.log(computerPlay());
